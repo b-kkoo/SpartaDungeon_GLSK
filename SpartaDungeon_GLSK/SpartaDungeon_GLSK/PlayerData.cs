ecbe7dd38d388c3537fdbf4c86ad4d6487d57398
b-kkoo <bmcow331@naver.com>
2024-09-27 19:54:42 +0900

    Reapply "[Fix] Player.cs, PlayerData.cs 수정"
    This reverts commit 9836687247bed219f90f002586c17311f6361045.

--- SpartaDungeon_GLSK/SpartaDungeon_GLSK/PlayerData.cs
+++ SpartaDungeon_GLSK/SpartaDungeon_GLSK/PlayerData.cs
@@ -9,7 +9,7 @@ using System.Xml.Linq;
 
 namespace SpartaDungeon_GLSK
 {
-    internal class PlayerData
+    public class PlayerData
     {
         public string Name { get; set; }
         public Chad Chad { get; set; }
@@ -17,15 +17,51 @@ namespace SpartaDungeon_GLSK
         public int Hp { get; set; }
         public int Atk { get; set; }
         public int Def { get; set; }
+        public int CriticalRate { get; set; }
 
         public void SetLv1() //set lv1함수
         {
-            
+            //if (player가 Warrior 선택 시)
+            //{
+            //    Warrior 정보 불러오기
+            //}
+            //else if (player가 Archer 선택 시)
+            //{
+            //    Archer 정보 불러오기
+            //}
+            //else //player가 Mage 선택 시
+            //{
+            //    Mage 정보 불러오기
+            //}
         }
 
         public void LvUp() //lv up함수
         {
-
+            //Warrior의 경우
+            if (Chad == Chad.Warrior)
+            {
+                Lv++;
+                Hp+=10;
+                Atk+=1;
+                Def+=3;
+            }
+            //Archer의 경우
+            else if (Chad == Chad.Archer)
+            {
+                Lv++;
+                Hp+=5;
+                Atk+=2;
+                Def+=2;
+                CriticalRate++;
+            }
+            //Mage의 경우
+            else
+            {
+                Lv++;
+                Hp += 5;
+                Atk += 3;
+                Def += 1;
+            }
         }
     }
 }
