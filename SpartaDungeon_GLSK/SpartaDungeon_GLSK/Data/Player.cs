a75d85a61f855fa103e03efd4a7c83af29b7f63f
b-kkoo <bmcow331@naver.com>
2024-09-27 19:53:57 +0900

    [Fix] Player.cs, PlayerData.cs 수정

--- SpartaDungeon_GLSK/SpartaDungeon_GLSK/Data/Player.cs
+++ SpartaDungeon_GLSK/SpartaDungeon_GLSK/Data/Player.cs
@@ -11,11 +11,11 @@ namespace SpartaDungeon_GLSK.Data
 {
     public class JobData
     {
-        public static Job Warrior = new Job(Chad.Warrior, 100, 5, 10, 10, 1, 3);
+        public static Job Warrior = new Job(Chad.Warrior, 100, 5, 10, 30);
 
-        public static Job Archer = new Job(Chad.Archer, 80, 7, 7, 5, 2, 2);
+        public static Job Archer = new Job(Chad.Archer, 80, 7, 7, 30);
 
-        public static Job Mage = new Job(Chad.Mage, 70, 10, 5, 3, 3, 1);
+        public static Job Mage = new Job(Chad.Mage, 70, 10, 5, 30);
     }
 
     public class Job
@@ -24,20 +24,16 @@ namespace SpartaDungeon_GLSK.Data
         public int Hp { get; set; }
         public int Atk { get; set; }
         public int Def { get; set; }
-        public int Hp_lvup { get; set; }
-        public int Atk_lvup { get; set; }
-        public int Def_lvup { get; set; }       
+        public int CriticalRate { get; set; }
 
-        public Job(Chad _chad, int _hp, int _atk, int _def, int _HP_lvup, int _Atk_lvup, int _Def_lvup)
+        public Job(Chad _chad, int _hp, int _atk, int _def, int _criticalRate)
         {
             Chad = _chad;
             Hp = _hp;
             Atk = _atk;
             Def = _def;
-            Hp_lvup = _HP_lvup;
-            Atk_lvup = _Atk_lvup;
-            Def_lvup = _Def_lvup;
-        }       
+            CriticalRate = _criticalRate;
+        }
     }
 
     public enum Chad
