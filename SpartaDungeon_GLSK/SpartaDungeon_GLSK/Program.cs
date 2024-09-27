3ce818f5bdb5c18d309f8f5d4437eaf568290b9f
jsj2518 <joseph2518@naver.com>
2024-09-27 17:45:10 +0900

    [Add] 불러오기

--- SpartaDungeon_GLSK/SpartaDungeon_GLSK/Program.cs
+++ SpartaDungeon_GLSK/SpartaDungeon_GLSK/Program.cs
@@ -1,12 +1,17 @@
-﻿namespace SpartaDungeon_GLSK
+﻿using SpartaDungeon_GLSK.Data;
+
+namespace SpartaDungeon_GLSK
 {
     internal class Program
     {
         static SceneManager sceneManager = new SceneManager();
 
         public static SaveData saveData; //저장파일 (불러올때 json, 저장할때 게임 데이터로 초기화)
-        public static PlayerData playerData;
-        public static IngameData ingameData;
+        public static PlayerData playerData = new PlayerData();
+        public static IngameData ingameData = new IngameData();
+
+        public static ItemData itemData = new ItemData();
+        public static MonsterData monsterData = new MonsterData();
 
         static void Main(string[] args)
         {
--- SpartaDungeon_GLSK/SpartaDungeon_GLSK/Scene/Battle.cs
+++ SpartaDungeon_GLSK/SpartaDungeon_GLSK/Scene/Battle.cs
@@ -1,26 +1,30 @@
-﻿using System;
+﻿using SpartaDungeon_GLSK.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
+using static SpartaDungeon_GLSK.Scene.BattleScene;
 
 namespace SpartaDungeon_GLSK.Scene
 {
     internal class BattleScene
     {
+        private BattleTable battleTable;
         //배틀 진행 정보
         public class BattleTable
         {
             //아군 진영
-            //PlayerData.WorldPlayer[] Ally
+            public PlayerData[] Ally;
             //적군 진영
-            //IngameData.WorldMonster[] Hostile
+            public WorldMonster[] Hostile;
 
-            public BattleTable(/*PlayerData.WorldPlayer */)
+            public BattleTable(/*PlayerData playerData, WorldMonster[] enemies*/)
             {
-                //Ally = new PlayerData.WorldPlayer[1]
-                //Ally[0] = 
+                //Ally = new PlayerData[1]
+                //Ally[0] = playerData;
+                //Hostile = enemies;
             }
         }
 
@@ -31,10 +35,54 @@ namespace SpartaDungeon_GLSK.Scene
 
             int cheatActivated;
 
+            MonsterCode[] enemies = new MonsterCode[] { MonsterCode.CommonMonster1, MonsterCode.CommonMonster2};
+            battleTable = new BattleTable(PlayerData.WorldPlayer/*나중에 다대다 전추 구현 가능하면 PlayerData에 함수 추가*/, IngameData.GetWorldMonster(enemies));
+
             keyController.GetUserInput(keyFilter, out cheatActivated); //반환값 안받으면 입력버퍼 지우라는 뜻
-            Console.WriteLine("현재 위치 MainScene");
-            Console.WriteLine("Z를 눌러 Inventory Scene으로 이동~");
-            Console.WriteLine("X를 눌러 프로그램 종료!");
+
+            int screenTop = Console.GetCursorPosition().Top;
+            bool loop = true;
+            while (loop)
+            {   
+                if (battleTable.Hostile[0].HPCur > 0)
+                {
+                    Console.SetCursorPosition(screenTop, 50);
+                    Console.WriteLine($"LV {battleTable.Hostile[0].monster.level}   {battleTable.Hostile[0].monster.name}");
+                    Console.SetCursorPosition(screenTop + 1, 0);
+                    Console.WriteLine($"{battleTable.Hostile[0].HPCur} / {battleTable.Hostile[0].monster.HP}");
+                }
+                if (battleTable.Hostile[1].HPCur > 0)
+                {
+                    Console.SetCursorPosition(screenTop, 50);
+                    Console.WriteLine($"LV {battleTable.Hostile[1].monster.level}   {battleTable.Hostile[1].monster.name}");
+                    Console.SetCursorPosition(screenTop + 1, 50);
+                    Console.WriteLine($"{battleTable.Hostile[1].HPCur} / {battleTable.Hostile[1].monster.HP}");
+                }
+                Console.SetCursorPosition(screenTop + 5, 0);
+                Console.WriteLine($"LV {battleTable.Ally[0].name}");
+                Console.SetCursorPosition(screenTop + 6, 0);
+                Console.WriteLine($"{battleTable.Ally[0].HPCur} / {battleTable.Ally[0].HP}");
+                Console.SetCursorPosition(screenTop + 8, 0);
+                Console.WriteLine("Z를 눌러 적을 공격!");
+
+                Console.SetCursorPosition(screenTop + 50, 0);
+                Console.SetCursorPosition(screenTop, 0);
+
+                bool loop2 = true;
+                while (loop2)
+                {
+                    keyFilter = new ConsoleKey[] { ConsoleKey.Z };
+                    keyInput = keyController.GetUserInput(keyFilter, out cheatActivated);
+
+                    switch (keyInput)
+                    {
+                        case ConsoleKey.Z:
+                            next = Scenes.Test_Inventory;
+                            break;
+
+                    }
+                }
+            }
 
             while (true)
             {
@@ -48,7 +96,7 @@ namespace SpartaDungeon_GLSK.Scene
                         return true;
 
                     case ConsoleKey.X:
-                        next = Scenes.Test_Start; //false를 반환하는 순간 next Scene은 중요치 않음
+                        next = Scenes.Test_Main; //false를 반환하는 순간 next Scene은 중요치 않음
                         return false;
                 }
             }
