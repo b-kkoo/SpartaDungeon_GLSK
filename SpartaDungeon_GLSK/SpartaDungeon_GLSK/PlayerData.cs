3c6024fe50289913f4441633ed287805712e4e12
b-kkoo <bmcow331@naver.com>
2024-09-27 20:02:55 +0900

    [Add] 치명타 확률, LvUp 함수 추가

--- SpartaDungeon_GLSK/SpartaDungeon_GLSK/PlayerData.cs
+++ SpartaDungeon_GLSK/SpartaDungeon_GLSK/PlayerData.cs
@@ -25,10 +25,29 @@ namespace SpartaDungeon_GLSK
 
         public void LvUp() //lv up함수
         {
-            Lv ++;
-            Hp += Hp_lvup;
-            Atk += Atk_lvup;
-            Def += Def_lvup;
+            if (Chad == Chad.Warrior)
+            {
+                Lv++;
+                Hp += 10;
+                Atk += 1;
+                Def += 3;
+            }
+            else if (Chad == Chad.Archer)
+            {
+                Lv++;
+                Hp += 5;
+                Atk += 2;
+                Def += 2;
+                CriRate++;
+            }
+            else
+            {
+                Lv++;
+                Hp += 5;
+                Atk += 3;
+                Def += 1;
+            }
+
         }
     }
 }
