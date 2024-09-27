06786445e2bca913802c8b8c9affba6d4e6e9eb8
jsj2518 <joseph2518@naver.com>
2024-09-27 11:07:00 +0900

    [Add] 씬 프리셋.cs
    씬 내에서 반복되는 부분을 프리셋 함수로 구현

--- SpartaDungeon_GLSK/SpartaDungeon_GLSK/DataManager.cs
+++ SpartaDungeon_GLSK/SpartaDungeon_GLSK/DataManager.cs
@@ -1,6 +1,53 @@
-﻿namespace SpartaDungeon_GLSK
+﻿using System.Runtime.CompilerServices;
+using System.Text.Json;
+
+namespace SpartaDungeon_GLSK
 {
-    internal class DataManager
+    public class DataManager
     {
+
+
+
+
+        //저장파일 경로는 실행파일과 같음
+        //저장파일 이름은 SaveData1, SaveData2, SaveData3 세개
+        /*public static bool LoadData(int saveSocket, out PlayerData playerData)
+        {
+            playerData = null;
+
+            if (saveSocket < 1 || saveSocket > 3)
+            {
+                return false;
+            }
+
+            string filePath = $"SaveData{saveSocket}";
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    // JSON 파일을 읽어서 객체로 역직렬화
+                    string jsonString = File.ReadAllText(filePath);
+                    playerData = JsonSerializer.Deserialize<PlayerData>(jsonString);
+                    Console.WriteLine($"\n - {saveSocket}번 데이터 불러오기 완료!\n");
+
+                    return playerData;
+                }
+                else
+                {
+                    Console.SetCursorPosition(0, Const.screenH + 10);
+                    Console.WriteLine("파일을 찾을 수 없습니다.");
+                    Console.SetCursorPosition(0, 0);
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.SetCursorPosition(0, Const.screenH + 10);
+                Console.WriteLine("데이터 불러오기 중 오류가 발생했습니다: " + ex.Message);
+                Console.SetCursorPosition(0, 0);
+                return null;
+            }
+        }*/
+        
     }
 }
