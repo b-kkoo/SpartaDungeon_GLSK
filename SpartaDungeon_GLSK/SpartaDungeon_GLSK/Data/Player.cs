b540a6262a6d1d0e4c229608eb0a6e506979a39d
b-kkoo <bmcow331@naver.com>
2024-09-27 16:47:33 +0900

    [Fix] Player.cs 수정

--- SpartaDungeon_GLSK/SpartaDungeon_GLSK/Data/Player.cs
+++ SpartaDungeon_GLSK/SpartaDungeon_GLSK/Data/Player.cs
@@ -22,6 +22,10 @@ namespace SpartaDungeon_GLSK.Data
             list.Add(new Mage("이름", Chad.Mage, 1, 100, 10, 5));
         }
         
+        public Player GetPlayer(Chad code)
+        {
+
+        }
     }
 
     public class Player
@@ -31,12 +35,9 @@ namespace SpartaDungeon_GLSK.Data
         public int Lv { get; set; }
         public int Hp { get; set; }
         public int Atk { get; set; }
-        public int Def { get; set; }                
-    }
-
-    public class Warrior : Player
-    {
-        public Warrior(string _name, Chad _chad, int _lv, int _hp, int _atk, int _def) 
+        public int Def { get; set; }
+        
+        public Player(string _name, Chad _chad, int _lv, int _hp, int _atk, int _def)
         {
             Name = _name;
             Chad = _chad;
@@ -47,23 +48,10 @@ namespace SpartaDungeon_GLSK.Data
         }
         public void LevelUpW()
         {
-            Lv ++;
+            Lv++;
             Hp += 10;
             Atk += 1;
             Def += 3;
-        }
-    }
-
-    public class Archer : Player 
-    {
-        public Archer(string _name, Chad _chad, int _lv, int _hp, int _atk, int _def)
-        {
-            Name = _name;
-            Chad = _chad;
-            Lv = _lv;
-            Hp = _hp;
-            Atk = _atk;
-            Def = _def;
         }
         public void LevelUpA()
         {
@@ -71,19 +59,6 @@ namespace SpartaDungeon_GLSK.Data
             Hp += 5;
             Atk += 2;
             Def += 2;
-        }
-    }
-
-    public class Mage : Player
-    {
-        public Mage(string _name, Chad _chad, int _lv, int _hp, int _atk, int _def)
-        {
-            Name = _name;
-            Chad = _chad;
-            Lv = _lv;
-            Hp = _hp;
-            Atk = _atk;
-            Def = _def;
         }
         public void LevelUpM()
         {
