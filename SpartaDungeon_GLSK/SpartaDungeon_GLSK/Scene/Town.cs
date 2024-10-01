b719e9d90337336ae016109a16ea0dd2a72670cd
b-kkoo <bmcow331@naver.com>
2024-10-01 20:12:47 +0900

    [Fix] 테스트항목 일부 삭제

--- SpartaDungeon_GLSK/SpartaDungeon_GLSK/Scene/Town.cs
+++ SpartaDungeon_GLSK/SpartaDungeon_GLSK/Scene/Town.cs
@@ -12,35 +12,6 @@
 
             keyController.GetUserInput(keyFilter, out cheatActivated);
 
-            ////이름 입력, 직업 선택, SetLv1 잘 되었는지 확인
-            //Console.WriteLine($"{Program.playerData.Name}"); //이름
-            //Console.WriteLine($"{Program.playerData.PClassName}"); //직업
-            //Console.WriteLine($"{Program.playerData.Lv}"); //레벨
-            //Console.WriteLine($"{Program.playerData.Hp}"); //HP
-            //Console.WriteLine($"{Program.playerData.Atk}"); //Atk
-            //Console.WriteLine($"{Program.playerData.MAtk}"); //MAtk
-            //Console.WriteLine($"{Program.playerData.Def}"); //Def
-            //Console.WriteLine($"{Program.playerData.Speed}"); //Speed
-            //Console.WriteLine($"{Program.playerData.CriRate}"); //CriRate
-
-            keyController.SetCheat(1, "show me the money");
-            keyFilter = new ConsoleKey[] { ConsoleKey.D1, ConsoleKey.D2, ConsoleKey.D3, ConsoleKey.D4, ConsoleKey.D5, ConsoleKey.D6, ConsoleKey.D7, ConsoleKey.D8 };
-            while (true)
-            {
-                keyInput = keyController.GetUserInput(keyFilter, out cheatActivated);
-                if( cheatActivated == 1)
-                {
-                    Program.playerData.Lv++;
-                    Console.WriteLine($"{Program.playerData.Lv}");
-                }
-            }
-            
-
-            
-
-
-
-
             Console.WriteLine("마을에 오신 것을 환영합니다.");
             Console.WriteLine("마을에서는 원하는 곳으로 이동할 수 있습니다.\n");
             Console.WriteLine($"1. 게임 메뉴");
