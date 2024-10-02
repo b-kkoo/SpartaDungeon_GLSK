0bb1bd3b9159c43ef0ba3589c62d1f5f51164e30
b-kkoo <bmcow331@naver.com>
2024-10-02 20:43:06 +0900

    [Add] Guild.cs 추가

---
+++ SpartaDungeon_GLSK/SpartaDungeon_GLSK/Scene/Guild.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaDungeon_GLSK.Scene
+{
+    internal class Guild
+    {
+        //길드
+        public static void GuildScene() 
+        {
+            Console.WriteLine("< 길드 >");
+            Console.WriteLine("퀘스트를 받거나 완료하고, 용병을 모집할 수 있습니다.");
+
+            //퀘스트
+
+            //용병 모집
+        }
+        
+        //퀘스트
+        public static void GuildQuest()
+        {
+
+        }
+
+        //용병 모집
+        public static void GuildMercenary()
+        {
+
+        }
+    }
+}
