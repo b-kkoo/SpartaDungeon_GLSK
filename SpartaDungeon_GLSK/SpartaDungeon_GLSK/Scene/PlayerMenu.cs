9166fafddc545b4a18bc809af6a57a412e8a60b8
b-kkoo <bmcow331@naver.com>
2024-10-02 14:18:21 +0900

    [Add] 상태창, 인벤토리, 장비관리, 스킬관리 추가

--- SpartaDungeon_GLSK/SpartaDungeon_GLSK/Scene/PlayerMenu.cs
+++ SpartaDungeon_GLSK/SpartaDungeon_GLSK/Scene/PlayerMenu.cs
@@ -62,5 +62,125 @@ namespace SpartaDungeon_GLSK.Scene
                 }
             }
         }
+
+        //상태창
+        public static bool Status(out Scenes next, KeyController keyController) 
+        {
+            ConsoleKey[] keyFilter = new ConsoleKey[] { ConsoleKey.NoName };
+            ConsoleKey keyInput;
+
+            int cheatActivated;
+
+            keyController.GetUserInput(keyFilter, out cheatActivated);
+
+            Console.WriteLine("< 상태창 >");
+            Console.WriteLine("플레이어의 정보를 확인할 수 있습니다.\n");
+            Console.WriteLine("- 이름    :");
+            Console.WriteLine("- 직업    :");
+            Console.WriteLine("- Lv      :");
+            Console.WriteLine("- Exp     :");
+            Console.WriteLine("- Hp      :");
+            Console.WriteLine("- Mp      :");
+            Console.WriteLine("- Atk     :");
+            Console.WriteLine("- MAtk    :");
+            Console.WriteLine("- Def     :");
+            Console.WriteLine("- Speed   :");
+            Console.WriteLine("- CriRate :");
+            Console.WriteLine("                                                             X : 뒤로");
+
+            while (true)
+            {
+                keyFilter = new ConsoleKey[] { ConsoleKey.X };
+                keyInput = keyController.GetUserInput(keyFilter, out cheatActivated);
+
+                switch (keyInput)
+                {
+                    case ConsoleKey.X:
+                        next = Scenes.PlayerMenu_Menu; //게임 메뉴로 이동
+                        return true;
+                }
+            }
+        }
+
+        //인벤토리
+        public static bool Inventory(out Scenes next, KeyController keyController)
+        {
+            ConsoleKey[] keyFilter = new ConsoleKey[] { ConsoleKey.NoName };
+            ConsoleKey keyInput;
+
+            int cheatActivated;
+
+            keyController.GetUserInput(keyFilter, out cheatActivated);
+
+            Console.WriteLine("< 인벤토리 >");
+            Console.WriteLine("소모 아이템 목록을 확인하고 소모 아이템을 사용할 수 있습니다.\n");
+
+            while (true)
+            {
+                keyFilter = new ConsoleKey[] { ConsoleKey.D1, ConsoleKey.D2, ConsoleKey.D3, ConsoleKey.D4, ConsoleKey.D5, ConsoleKey.X };
+                keyInput = keyController.GetUserInput(keyFilter, out cheatActivated);
+
+                switch (keyInput)
+                {
+                    case ConsoleKey.X:
+                        next = Scenes.PlayerMenu_Menu; //게임 메뉴로 이동
+                        return true;
+                }
+            }
+        }
+
+        //장비
+        public static bool Equipment(out Scenes next, KeyController keyController)
+        {
+            ConsoleKey[] keyFilter = new ConsoleKey[] { ConsoleKey.NoName };
+            ConsoleKey keyInput;
+
+            int cheatActivated;
+
+            keyController.GetUserInput(keyFilter, out cheatActivated);
+
+            Console.WriteLine("< 게임 메뉴 >");
+            Console.WriteLine("장비 아이템 목록을 확인하고 장착/해제할 수 있습니다.\n");
+
+            while (true)
+            {
+                keyFilter = new ConsoleKey[] { ConsoleKey.D1, ConsoleKey.D2, ConsoleKey.D3, ConsoleKey.D4, ConsoleKey.D5, ConsoleKey.X };
+                keyInput = keyController.GetUserInput(keyFilter, out cheatActivated);
+
+                switch (keyInput)
+                {
+                    case ConsoleKey.X:
+                        next = Scenes.PlayerMenu_Menu; //게임 메뉴로 이동
+                        return true;
+                }
+            }
+        }
+
+        //스킬
+        public static bool Skill(out Scenes next, KeyController keyController)
+        {
+            ConsoleKey[] keyFilter = new ConsoleKey[] { ConsoleKey.NoName };
+            ConsoleKey keyInput;
+
+            int cheatActivated;
+
+            keyController.GetUserInput(keyFilter, out cheatActivated);
+
+            Console.WriteLine("< 게임 메뉴 >");
+            Console.WriteLine("스킬 목록과 스킬 정보를 확인할 수 있습니다.\n");
+
+            while (true)
+            {
+                keyFilter = new ConsoleKey[] { ConsoleKey.D1, ConsoleKey.D2, ConsoleKey.D3, ConsoleKey.D4, ConsoleKey.D5, ConsoleKey.X };
+                keyInput = keyController.GetUserInput(keyFilter, out cheatActivated);
+
+                switch (keyInput)
+                {
+                    case ConsoleKey.X:
+                        next = Scenes.PlayerMenu_Menu; //게임 메뉴로 이동
+                        return true;
+                }
+            }
+        }
     }
 }
