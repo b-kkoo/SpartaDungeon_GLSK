3c3705326a509b0e30a4d8fe980db260ae7406a1
altkzs <dogmagoa@gmail.com>
2024-10-02 16:31:32 +0900

    아이템,게임오버

--- SpartaDungeon_GLSK/SpartaDungeon_GLSK/Data/Item.cs
+++ SpartaDungeon_GLSK/SpartaDungeon_GLSK/Data/Item.cs
@@ -19,24 +19,41 @@ namespace SpartaDungeon_GLSK.Data
         {
             Gears = new Dictionary<GearCode, Gear>();
 
-            //        코드(중복X!!)                     이름            타입                   Atk    Def
-            Gears.Add(GearCode.Sword1,      new Gear(   "검1",          GearType.WeaponS,      5,     0));
-            Gears.Add(GearCode.Sword2,      new Gear(   "검2",          GearType.WeaponS,      7,     0));
+            //        코드(중복X!!)                     이름               타입                    hp     mp    Atk    mAtk    Def  speed criRate Price
+            Gears.Add(GearCode.Sword1,      new Gear(   "나무 검",       GearType.WeaponS,     0,     0,      5,     3,     0,     5,      10,   30));
+            Gears.Add(GearCode.Sword2,      new Gear(   "구리 검",       GearType.WeaponS,     0,     0,      8,     6,     0,     8,      25,   55));
+            Gears.Add(GearCode.Sword3,      new Gear(   "철 검",         GearType.WeaponS,     0,     0,     11,     9,     0,     11,     40,  85));
+            Gears.Add(GearCode.Sword4,      new Gear(   "엑스칼리버",      GearType.WeaponS,     0,     0,     28,    20,     0,    20,     60,  700));
 
-            Gears.Add(GearCode.Bow1,        new Gear(   "활1",          GearType.WeaponB,      6,     0));
-            Gears.Add(GearCode.Bow2,        new Gear(   "활2",          GearType.WeaponB,      8,     0));
+            Gears.Add(GearCode.Bow1,        new Gear(   "숏 보우",        GearType.WeaponB,     0,     0,      6,     5,     0,     4,     20,   35));
+            Gears.Add(GearCode.Bow2,        new Gear(   "롱 보우",        GearType.WeaponB,     0,     0,      8,     8,     0,     8,     30,   55));
+            Gears.Add(GearCode.Bow3,        new Gear(   "사냥꾼의 활",     GearType.WeaponB,     0,     0,     14,    13,     0,    15,     40,   85));
+            Gears.Add(GearCode.Bow4,        new Gear(   "아그니 간디바",    GearType.WeaponB,     0,     0,     28,    20,     0,    20,     60,  700));
 
-            Gears.Add(GearCode.Wand1,       new Gear(   "지팡이1",      GearType.WeaponW,      7,     0));
-            Gears.Add(GearCode.Wand2,       new Gear(   "지팡이2",      GearType.WeaponW,      9,     0));
+            Gears.Add(GearCode.Wand1,       new Gear(   "우드 스태프",     GearType.WeaponW,     0,     0,      7,     5,     0,     4,      20,   35));
+            Gears.Add(GearCode.Wand2,       new Gear(   "사파이어 스태프",  GearType.WeaponW,     0,     0,      9,     7,     0,     8,      30,   55));
+            Gears.Add(GearCode.Wand3,       new Gear(   "에메랄드 스태프",  GearType.WeaponW,     0,     0,      7,     5,     0,     15,     40,   85));
+            Gears.Add(GearCode.Wand4,       new Gear(   "레바테인",        GearType.WeaponW,    0,     0,     28,    20,     0,     20,     60,  700));
 
-            Gears.Add(GearCode.HeavyArmor1, new Gear(   "중갑옷1",      GearType.ArmorHA,      0,     7));
-            Gears.Add(GearCode.HeavyArmor2, new Gear(   "중갑옷2",      GearType.ArmorHA,      0,     9));
+            Gears.Add(GearCode.HeavyArmor1, new Gear(   "철갑옷",         GearType.ArmorHA,     5,     0,      0,     1,      7,     0,     0,   45));
+            Gears.Add(GearCode.HeavyArmor2, new Gear(   "강철갑옷",        GearType.ArmorHA,    10,     0,      0,     3,      9,     0,     0,   65));
+            Gears.Add(GearCode.HeavyArmor3, new Gear(   "그림자갑옷",      GearType.ArmorHA,     15,     0,      0,    5,     12,     0,     0,   85));
+            Gears.Add(GearCode.HeavyArmor4, new Gear(   "드래곤뼈갑옷",     GearType.ArmorHA,    24,     0,      0,     10,    24,     0,     0,  700));
+             
+            Gears.Add(GearCode.LightArmor1, new Gear(   "가죽갑옷",       GearType.ArmorLA,     0,     0,      0,     2,     6,      0,       0,   45));
+            Gears.Add(GearCode.LightArmor2, new Gear(   "쇠사슬갑옷",      GearType.ArmorLA,     0,     0,      0,     4,     8,      0,      0,   65));
+            Gears.Add(GearCode.LightArmor3, new Gear(   "와이번가죽갑옷",   GearType.ArmorLA,     0,     0,      0,     6,     12,     0,      0,   85));
+            Gears.Add(GearCode.LightArmor4, new Gear(   "드래곤가죽갑옷",   GearType.ArmorLA,     0,     0,      0,    10,     18,     0,      0,  700));
 
-            Gears.Add(GearCode.LightArmor1, new Gear(   "경갑옷1",      GearType.ArmorLA,      0,     6));
-            Gears.Add(GearCode.LightArmor2, new Gear(   "경갑옷2",      GearType.ArmorLA,      0,     8));
-
-            Gears.Add(GearCode.Robe1,       new Gear(   "로브1",        GearType.ArmorR,       0,     5));
-            Gears.Add(GearCode.Robe2,       new Gear(   "로브2",        GearType.ArmorR,       0,     7));
+            Gears.Add(GearCode.Robe1,       new Gear(   "견습로브",        GearType.ArmorR,     5,     0,      0,     5,      5,      0,     0,   45));
+            Gears.Add(GearCode.Robe2,       new Gear(   "마법사로브",       GearType.ArmorR,     7,     0,      0,     7,      7,      0,     0,   65));
+            Gears.Add(GearCode.Robe3,       new Gear(   "대마법사로브",     GearType.ArmorR,     9,     0,      0,     10,      9,     0,     0,   85));
+            Gears.Add(GearCode.Robe4,       new Gear(   "현자로브",        GearType.ArmorR,    15,     0,      0,     20,     15,     0,     0,  700));
+ 
+            Gears.Add(GearCode.Ring1,       new Gear(   "마나반지",        GearType.Ring,       0,     8,      0,     6,     4,     0,      0,   50));
+            Gears.Add(GearCode.Ring2,       new Gear(   "활력반지",        GearType.Ring,       8,     0,      0,     6,     4,     0,      0,   65));
+            Gears.Add(GearCode.Ring3,       new Gear(   "시작반지",        GearType.Ring,      10,    10,      0,     6,     4,     0,      0,   80));
+            Gears.Add(GearCode.Ring4,       new Gear(   "만능반지",        GearType.Ring,      20,    20,      20,   20,    20,     0,      0,  700));
         }
 
         public static Gear GetGear(GearCode code)
@@ -57,6 +74,7 @@ namespace SpartaDungeon_GLSK.Data
         public string name { get; }
         public string info { get; }
         public GearType type { get; }
+        public int Price { get; } 
 
         //장비 옵션
         public int hp { get; }
@@ -68,12 +86,20 @@ namespace SpartaDungeon_GLSK.Data
         public int criRate { get; }
 
 
-        public Gear(string _name, GearType _type, int _atk, int _def)
+        public Gear(string _name, GearType _type, int _hp, int _mp, int _atk, int _mAtk, int _def, int _speed, int _criRate, int _Price)
         {
             name = _name;
             type = _type;
+            hp = _hp;
+            mp = _mp;
             atk = _atk;
+            mAtk = _mAtk;
             def = _def;
+            speed = _speed;
+            criRate = _criRate;
+            Price = _Price;
+
+
         }
     }
 
@@ -82,16 +108,38 @@ namespace SpartaDungeon_GLSK.Data
     {
         Sword1,
         Sword2,
+        Sword3,
+        Sword4,
+
         Bow1,
         Bow2,
+        Bow3,
+        Bow4,
+
         Wand1,
         Wand2,
+        Wand3,
+        Wand4,
+
         HeavyArmor1,
         HeavyArmor2,
+        HeavyArmor3,
+        HeavyArmor4,
+
         LightArmor1,
         LightArmor2,
+        LightArmor3,
+        LightArmor4,
+
         Robe1,
-        Robe2
+        Robe2,
+        Robe3,
+        Robe4,
+
+        Ring1,
+        Ring2,
+        Ring3,
+        Ring4
     }
 
     //아이템 타입
@@ -134,9 +182,11 @@ namespace SpartaDungeon_GLSK.Data
         {
             Potions = new Dictionary<PotionCode, Potion>();
 
-            //          코드(중복X!!)                   이름          타입                    power
-            Potions.Add(PotionCode.Potion1, new Potion("포션1",       PotionType.HP,    20));
-            Potions.Add(PotionCode.Potion2, new Potion("포션2",       PotionType.HP,    50));
+            //          코드(중복X!!)                   이름          타입               power  Price
+            Potions.Add(PotionCode.Potion1, new Potion("체력포션",       PotionType.HP,    20,     00));
+            Potions.Add(PotionCode.Potion2, new Potion("체력포션",       PotionType.HP,    50,     00));
+            Potions.Add(PotionCode.Potion1, new Potion("마나포션",       PotionType.MP,    10,     00));
+            Potions.Add(PotionCode.Potion2, new Potion("마나포션",       PotionType.MP,    30,     00));
 
             //아이템을 계속 추가해 보자
         }
@@ -159,14 +209,16 @@ namespace SpartaDungeon_GLSK.Data
         public string name { get; }
         public PotionType type { get; }
         public int power { get; }
+        public int Price {get;}
 
         //필드를 계속 추가해 보자
 
-        public Potion(string _name, PotionType _type, int _atk)
+        public Potion(string _name, PotionType _type, int _atk, int _Price)
         {
             name = _name;
             type = _type;
             power = _atk;
+            Price = _Price;
         }
     }
 
---
+++ SpartaDungeon_GLSK/SpartaDungeon_GLSK/Scene/GameOver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace SpartaDungeon_GLSK.Scene
+{
+    internal class GameoverScene
+	{
+        public static bool GAMEOVER(out Scenes next, KeyController keyController)
+        {
+            ConsoleKey[] keyFilter = new ConsoleKey[] { ConsoleKey.NoName };
+            ConsoleKey keyInput;
+
+            int cheatActivated;
+
+            keyController.GetUserInput(keyFilter, out cheatActivated);
+
+            Console.WriteLine("YOU DIED");
+            Console.WriteLine("당신은 사망하셨습니다\n");
+            Console.WriteLine("1. 마을로");
+            Console.WriteLine("2. 나가기");
+            Console.WriteLine("                                               숫자 버튼을 눌러 선택!");
+            while (true)
+            {
+                keyFilter = new ConsoleKey[] { ConsoleKey.D1, ConsoleKey.D2};
+                keyInput = keyController.GetUserInput(keyFilter, out cheatActivated);
+
+                switch (keyInput)
+                {
+                    case ConsoleKey.D1:
+                        next = Scenes.Town_Default; //마을로 이동
+                        return true;
+
+                    case ConsoleKey.D2:
+                        next = Scenes.Main_Menu; //메인메뉴로 이동
+                        return true;
+                }
+            }
+        }
+    }
+}
+
+
