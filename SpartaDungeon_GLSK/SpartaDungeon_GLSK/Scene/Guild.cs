1d76a284dfcfbc16c7c2878a768856c43f429af6
jsj2518 <joseph2518@naver.com>
2024-10-04 10:30:54 +0900

    [Add] 길드에서 모험가 영입

--- SpartaDungeon_GLSK/SpartaDungeon_GLSK/Scene/Guild.cs
+++ SpartaDungeon_GLSK/SpartaDungeon_GLSK/Scene/Guild.cs
@@ -1,16 +1,58 @@
 ﻿using SpartaDungeon_GLSK.Data;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SpartaDungeon_GLSK.Scene
 {
-    internal class GuildScene
+    public class GuildScene
     {
+        private static List<Mercenary> mercenaries;
+        //고용 모험가
+        private class Mercenary
+        {
+            public bool Purchased { get; set; }
+
+            public string Name { get; }
+            public JobCode PClass { get; }
+            public string PClassName { get; }
+            public int Lv { get; }
+
+            public int Price { get; }
+
+            public Mercenary(string name, JobCode pClass, string pClassName, int lv, int price)
+            {
+                Purchased = false;
+
+                Name = name;
+                PClass = pClass;
+                PClassName = pClassName;
+
+                Lv = lv;
+
+                Price = price;
+            }
+        }
+
+
+        public static void Set()
+        {
+            mercenaries = new List<Mercenary>();
+
+            mercenaries.Add(new Mercenary("도도", JobCode.Warrior, "전사", 1, 0));
+            mercenaries.Add(new Mercenary("바우", JobCode.Archer, "궁수", 1, 0));
+            mercenaries.Add(new Mercenary("델리키", JobCode.Mage, "마법사", 1, 0));
+            mercenaries.Add(new Mercenary("듀크", JobCode.Warrior, "전사", 5, 300));
+            mercenaries.Add(new Mercenary("파칼", JobCode.Archer, "궁수", 5, 300));
+            mercenaries.Add(new Mercenary("콜라비", JobCode.Mage, "마법사", 5, 300));
+        }
+
         //길드
-        public static bool GuildHall(out Scenes next, KeyController keyController) 
+        public static bool GuildHall(out Scenes next, KeyController keyController)
         {
             ConsoleKey[] keyFilter = new ConsoleKey[] { ConsoleKey.NoName };
             ConsoleKey keyInput;
@@ -121,7 +163,7 @@ namespace SpartaDungeon_GLSK.Scene
                     Console.WriteLine($"{allQuest.Count}. (...) {quest.name}");
                 }
 
-                keyFilter = new ConsoleKey[] { ConsoleKey.D1, ConsoleKey.D2, ConsoleKey.D3, ConsoleKey.D4, ConsoleKey.D5, ConsoleKey.X };
+                keyFilter = new ConsoleKey[] { ConsoleKey.D1, ConsoleKey.D2, ConsoleKey.D3, ConsoleKey.D4, ConsoleKey.D5, ConsoleKey.D6, ConsoleKey.D7, ConsoleKey.D8, ConsoleKey.D9, ConsoleKey.X };
                 bool loop2 = true;
                 while (loop2)
                 {
@@ -134,6 +176,10 @@ namespace SpartaDungeon_GLSK.Scene
                         case ConsoleKey.D3:
                         case ConsoleKey.D4:
                         case ConsoleKey.D5:
+                        case ConsoleKey.D6:
+                        case ConsoleKey.D7:
+                        case ConsoleKey.D8:
+                        case ConsoleKey.D9:
                             int selectedIdx = keyInput - ConsoleKey.D1;
                             if (selectedIdx < allQuest.Count)
                             {
@@ -194,9 +240,146 @@ namespace SpartaDungeon_GLSK.Scene
         }
 
         //모험가 모집
-        public static void GuildMercenary()
+        public static bool GuildMercenary(out Scenes next, KeyController keyController)
         {
+            ConsoleKey[] keyFilter = new ConsoleKey[] { ConsoleKey.NoName };
+            ConsoleKey keyInput;
 
+            int cheatActivated;
+            keyController.GetUserInput(keyFilter, out cheatActivated);
+
+            bool loop = true;
+            while (loop)
+            {
+                Console.Clear();
+
+                List<int> waitingMercenary = new List<int>();
+                for (int i = 0; i < mercenaries.Count; i++)
+                {
+                    mercenaries[i].Purchased = Program.ingameData.MercenaryPurchased[i];
+                    if (Program.ingameData.MercenaryPurchased[i] == false) waitingMercenary.Add(i);
+                }
+
+                Console.WriteLine(" < < 길드 > >\n");
+                Console.WriteLine(" - 모험가 모집\n\n");
+
+                Console.SetCursorPosition(20, 0);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write($"소지금 : {Program.playerData.Gold} Gold");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(0, 5);
+
+                if (waitingMercenary.Count == 0)
+                {
+                    string[] comment = new string[] { "영입할 수 있는 모험가가 더이상 없습니다." };
+                    ScenePreset.Conversation(comment, keyController);
+                    next = Scenes.Guild_Hall;
+                    return true;
+                }
+
+                //모험가 구매 목록 디스플레이
+                int waitingNum = 0;
+                for (int i = 0; i < waitingMercenary.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {mercenaries[waitingMercenary[i]].Name}, {mercenaries[waitingMercenary[i]].PClassName}, LV {mercenaries[waitingMercenary[i]].Lv} : {mercenaries[waitingMercenary[i]].Price} Gold");
+                }
+
+                keyFilter = new ConsoleKey[] { ConsoleKey.D1, ConsoleKey.D2, ConsoleKey.D3, ConsoleKey.D4, ConsoleKey.D5, ConsoleKey.D6, ConsoleKey.D7, ConsoleKey.D8, ConsoleKey.D9, ConsoleKey.X };
+                bool loop2 = true;
+                while (loop2)
+                {
+                    keyInput = keyController.GetUserInput(keyFilter, out cheatActivated);
+
+                    switch (keyInput)
+                    {
+                        case ConsoleKey.D1:
+                        case ConsoleKey.D2:
+                        case ConsoleKey.D3:
+                        case ConsoleKey.D4:
+                        case ConsoleKey.D5:
+                        case ConsoleKey.D6:
+                        case ConsoleKey.D7:
+                        case ConsoleKey.D8:
+                        case ConsoleKey.D9:
+                            int selectedIdx = keyInput - ConsoleKey.D1;
+                            if (selectedIdx < waitingMercenary.Count)
+                            {
+                                Mercenary mercenary = mercenaries[waitingMercenary[selectedIdx]];
+
+                                Console.SetCursorPosition(0, 5);
+                                for (int i = 0; i <= 25; i++) Console.WriteLine(new string(' ', Console.WindowWidth));
+                                Console.SetCursorPosition(0, 0);
+                                Console.SetCursorPosition(0, 5);
+                                Console.WriteLine($"{selectedIdx + 1}. {mercenary.Name}, {mercenary.PClassName}, LV {mercenary.Lv} : {mercenary.Price} Gold");
+
+                                Console.WriteLine("\n해당 모험가를 영입하시겠습니까?");
+                                Console.WriteLine("\n                                 (Z : 예,  X : 아니오)");
+
+                                keyFilter = new ConsoleKey[] { ConsoleKey.Z, ConsoleKey.X };
+                                bool loop3 = true;
+                                while (loop3)
+                                {
+                                    keyInput = keyController.GetUserInput(keyFilter, out cheatActivated);
+
+                                    switch (keyInput)
+                                    {
+                                        case ConsoleKey.Z:
+                                            Console.SetCursorPosition(0, 5);
+                                            for (int i = 0; i <= 25; i++) Console.WriteLine(new string(' ', Console.WindowWidth));
+                                            Console.SetCursorPosition(0, 0);
+                                            Console.SetCursorPosition(0, 5);
+
+                                            if (Program.playerData.Gold > mercenary.Price) //구매 완
+                                            {
+                                                //골드 차감
+                                                Program.playerData.Gold -= mercenary.Price;
+                                                //정보 갱신
+                                                Program.ingameData.MercenaryPurchased[waitingMercenary[selectedIdx]] = true;
+
+                                                //팀에 모험가 추가
+                                                PlayerUnitData playerUnitData = new PlayerUnitData();
+                                                playerUnitData.Name = mercenary.Name;
+                                                playerUnitData.PClass = mercenary.PClass;
+                                                playerUnitData.SetLv1();
+                                                for (int i = 1; i < mercenary.Lv; i++) playerUnitData.LvUp();
+                                                playerUnitData.Exp = 0;
+
+                                                Program.playerData.team.Add(playerUnitData);
+
+                                                //구매 메시지
+                                                Console.WriteLine($"{mercenary.Name}을 영입하였습니다!");
+                                                Console.WriteLine($"\n파티 정보에서 엔트리에 추가해 주세요.");
+                                                Thread.Sleep(1000);
+                                                keyController.GetUserInput(keyFilter, out cheatActivated);
+                                            }
+                                            else //골드 소지량 부족
+                                            {
+                                                Console.WriteLine("영입에 필요한 골드가 부족합니다.");
+                                                Thread.Sleep(1000);
+                                                keyController.GetUserInput(keyFilter, out cheatActivated);
+                                            }
+                                            loop3 = false;
+                                            break;
+
+                                        case ConsoleKey.X:
+                                            loop3 = false;
+                                            break;
+                                    }
+                                }
+                                loop2 = false;
+                            }
+                            break;
+
+                        case ConsoleKey.X:
+                            loop2 = false;
+                            loop = false;
+                            break;
+                    }
+                }
+            }
+
+            next = Scenes.Guild_Hall;
+            return true;
         }
     }
 }
